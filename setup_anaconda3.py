61daa30d7486f91fc902be55620a099a7f93c0fd
biomedswe <jonas870318@gmail.com>
2021-03-18 23:15:28 +0100

    made setup_anaconda3 an independent program

--- setup_anaconda3.py
+++ setup_anaconda3.py
@@ -21,7 +21,7 @@ class SetupAnaconda3():
             print('Installation of Anaconda3 allready completed, skips step...')
 
         else:
-            misc.clear_screen()
+            subprocess.run("clear", shell=True)
             print("Download and install Anaconda3\n\n\nDownloading and installing anaconda from https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh...")
 
             # download anaconda
