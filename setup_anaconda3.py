2ac8dcb687795425d97da4a8dbd51d8311f7fd8f
biomedswe <jonas870318@gmail.com>
2021-03-18 17:53:46 +0100

    setup is now an independent program

--- setup_anaconda3.py
+++ setup_anaconda3.py
@@ -1,17 +1,20 @@
 from os import sys, path, getenv
-import time
+from menus import Misc
+
 
 class SetupAnaconda3():
 
     def __init__(self):
         pass
 
-    def install_anaconda(self, misc, shortcuts):
+    def install_anaconda(self, misc):
         '''This function downloads anaconda via wget and the link-adress to the linux installer from anaconda.com.
            It then installs anaconda and creates an environment with the required software packages.
            Finally creates the directory three in the sequencing_project folder'''
 
-        if misc.step_allready_completed(shortcuts.anaconda_setup_complete):
+        setup_complete = getenv("HOME")+'/anaconda3/install.complete'
+
+        if misc.step_allready_completed(setup_complete):
             misc.logfile('Installation of Anaconda3 allready completed, skips step...')
 
         else:
@@ -24,9 +27,9 @@ class SetupAnaconda3():
             misc.logfile('Anaconda3 succesfully downloaded')
 
             # applies read, write and execute permission for all users to the file
-            cmd_chmod = "chmod a=xrw $HOME/Anaconda3-2020.11-Linux-x86_64.sh"
-            misc.run_command(cmd_chmod)
-            misc.logfile('Read, write and execute permission for Anaconda3-2020.11-Linux-x86_64.sh succesfully applied')
+            # cmd_chmod = "chmod a=xrw $HOME/Anaconda3-2020.11-Linux-x86_64.sh"
+            # misc.run_command(cmd_chmod)
+            # misc.logfile('Read, write and execute permission for Anaconda3-2020.11-Linux-x86_64.sh succesfully applied')
 
             # installs anaconda
             cmd_install = "bash $HOME/Anaconda3-2020.11-Linux-x86_64.sh"
@@ -34,10 +37,10 @@ class SetupAnaconda3():
             misc.logfile('Anaconda3 succesfully installed')
 
             # create file that shows if anaconda3 is allready installed
-            misc.create_trackFile(shortcuts.anaconda_setup_complete)
-            misc.logfile(f'Trackfile {shortcuts.anaconda_setup_complete} succesfully created')
+            misc.create_trackFile(setup_complete)
+            misc.logfile('Trackfile {0} succesfully created'.format(setup_complete))
 
-            print("\nAnaconda is now successfully installed\n\n")
+            misc.logfile("\nAnaconda is now successfully installed\n\n")
             input("The terminal must be restarted for anaconda3 to initialize correctly\n\nPress any key to close the terminal\nThen start it again manually")
             misc.close_terminal()
 
@@ -64,12 +67,14 @@ class SetupAnaconda3():
             misc.logfile('Succesfully created directory tree')
 
             if path.isdir(getenv("HOME")+'/anaconda3/envs/sequencing'):
-                print("Environment allready exists, do you want to owerwrite?\n")
+                misc.logfile("Environment allready exists, do you want to owerwrite?\n")
                 if not misc.confirm_choice():
+                    misc.logfile('User input: No')
                     return
 
             misc.clear_screen()
-            print('''\033[1mSet up a new conda environment for DNA and RNA-sequence analysis.\033[0m
+            misc.logfile('User input: Yes')
+            misc.logfile('''\033[1mSet up a new conda environment for DNA and RNA-sequence analysis.\033[0m
 
 Creating a new environment named \"Secuencing\"...
 
@@ -92,13 +97,15 @@ STAR
 ''')
 
             cmd_env = "conda create -n sequencing -c bioconda bedtools bcftools biopython bwa gatk4 picard python=3.7.6 samtools=1.9 star pandas vcfpy scipy snpeff"
-            misc.run_command(cmd_env, None)
+            misc.run_command(cmd_env)
+            misc.logfile('{0} - OK!'.format(cmd_env))
 
             # Delly in bioconda didn't work so I had to do a workaround
             cmd_download_delly = "wget https://github.com/dellytools/delly/releases/download/v0.8.7/delly_v0.8.7_linux_x86_64bit -P $HOME/anaconda3/envs/sequencing/bin"
-            misc.run_command(cmd_download_delly, "Download Delly completed")
+            misc.run_command(cmd_download_delly)
+            misc.logfile('Download delly - OK!')
             cmd_chmod_delly = "chmod a=xrw $HOME/anaconda3/envs/sequencing/bin/delly_v0.8.7_linux_x86_64bit"
-            misc.run_command(cmd_chmod_delly, "Change permission for delly")
+            misc.run_command(cmd_chmod_delly)
             cmd_mv = "mv $HOME/anaconda3/envs/sequencing/bin/delly_v0.8.7_linux_x86_64bit $HOME/anaconda3/envs/sequencing/bin/delly"
             misc.run_command(cmd_mv, "Move Delly completed")
 
@@ -115,4 +122,13 @@ STAR
             input("Press any key to exit program...")
             sys.exit()
         except Exception as e:
-            print(f'Error with create_anaconda_environment(): {e}')
+            misc.logfile('Error with create_anaconda_environment(): {0}'.format(e))
+
+
+def main():
+    setup = SetupAnaconda3()
+    misc = Misc()
+    misc.logfile('Download and install Anaconda3 with python 3.7.6')
+    setup.install_anaconda(misc)
+if __name__ == '__main__':
+    main()
